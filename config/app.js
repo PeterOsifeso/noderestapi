42341a03099db79d52dfb7abbf9125730748a35c
Peter Osifeso <peterosifeso@gmail.com>
2016-12-12 19:42:15 +0100

    Initial commit

---
+++ config/app.js
@@ -0,0 +1,10 @@
+var config = {
+    host: 'http://localhost',
+    db: {
+        host: 'localhost',
+        database: 'test',
+        user: 'root',
+        password: ''
+    }
+};
+module.exports = config;
